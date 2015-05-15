739e4777a80eef8ae545f9573a177889db1ea51f
kvarona <kristen@torak.com>
2015-05-15 11:37:50 -0700

    updated repo location updated pom

--- src/test/java/test/atf/toolbox/ATFHandlerTest.java
+++ src/test/java/test/atf/toolbox/ATFHandlerTest.java
@@ -1,10 +1,25 @@
 package test.atf.toolbox;
 
 
-// import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.path.xml.XmlPath;
+import com.jayway.restassured.path.xml.element.NodeChildren;
+
+
+
 public class ATFHandlerTest {
 
-	// Placeholder class to exericise the framework during development DO NOT REMOVE !!!
+	// Placeholder class to exercise the framework during development DO NOT REMOVE !!!
 
-
+	
 }
